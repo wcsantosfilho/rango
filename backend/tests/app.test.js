986b69fc9df5324f98ffb129deb2ee980d05ae7a
Walter Santos Filho <wcsantosfilho@gmail.com>
2019-09-07 15:50:51 -0300

    Acertos dos testes

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -13,7 +13,7 @@ const app = require('../src/loader')
             callback - tests to be done
   * @return ???
 */
-test('#A - GET de: /api/registroEntrega/', function (t) {
+test('#A - POST de: /api/registroEntrega/', function (t) {
     // Chama app.server porque foram exportados "server" e "db"
     // para que o teste possa fechar a conexao do db ao final e 
     // o teste não ficar suspenso
@@ -25,17 +25,20 @@ test('#A - GET de: /api/registroEntrega/', function (t) {
         ]
     })
     .expect('Content-Type', /json/)
-    .expect(200)
+    .expect(201)
     .end((err, res) => {
         var actualThings = res.body
         // TESTE 1
-        t.error(err, 'Sem erros na execução do GET')
+        t.error(err, 'Sem erros na execução do POST')
         // TESTE 2
-        //t.same(actualThings, expectedThings, 'A lista de registros de entrega é trazida corretamente')
+        var entrega0 = "Refeições"
+        var qtde0 = 150
+        t.same(res.body.items[0].itemEntrega, entrega0, 'A lista de registros de entrega foi gravada corretamente')
+        t.same(res.body.items[0].quantidadeEntrega, qtde0, 'A lista de registros de entrega foi gravada corretamente')
         
         // Fecha a conexão com o banco para encerrar o teste, 
         // caso contrário ficava suspenso
-        app.db.connection.close()
+        //app.db.connection.close()
         t.end()
     })
 })
@@ -57,26 +60,15 @@ test('#B - GET de: /api/registroEntrega/', function (t) {
     .expect('Content-Type', /json/)
     .expect(200)
     .end((err, res) => {
-        var expectedThings = [
-            {"_id":"5c3d2660313d1c069074419a","dataEntrega":"2019-01-14T22:00:00.000Z","__v":0,
-                "items":[
-                    {"itemEntrega":" Refeições","quantidadeEntrega":150,"_id":"5c3d2660313d1c069074419c"},
-                    {"itemEntrega":" Sucos","quantidadeEntrega":80,"_id":"5c3d2660313d1c069074419b"}
-                ]
-            },
-            {"_id":"5c3d26cf313d1c06907441a1","dataEntrega":"2019-01-13T22:00:00.000Z","__v":0,
-                "items":[
-                    {"itemEntrega":" Refeições","quantidadeEntrega":202,"_id":"5c3d26cf313d1c06907441a4"},
-                    {"itemEntrega":" Roupas","quantidadeEntrega":55,"_id":"5c3d26cf313d1c06907441a3"},
-                    {"itemEntrega":" Sapatos","quantidadeEntrega":12,"_id":"5c3d26cf313d1c06907441a2"}
-                ]
-            }
-        ]
         var actualThings = res.body
+        console.log(res.body[0].items[0]);
         // TESTE 1
         t.error(err, 'Sem erros na execução do GET')
         // TESTE 2
-        t.same(actualThings, expectedThings, 'A lista de registros de entrega é trazida corretamente')
+        var entrega0 = "Refeições"
+        var qtde0 = 150
+        t.same(res.body[0].items[0].itemEntrega, entrega0, 'A lista de registros de entrega foi lida corretamente')
+        t.same(res.body[0].items[0].quantidadeEntrega, qtde0, 'A lista de registros de entrega foi lida corretamente')
         
         // Fecha a conexão com o banco para encerrar o teste, 
         // caso contrário ficava suspenso
